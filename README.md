30de2114bb57caae31ff2a5365e8191ce1ca70e5
Huy Nguyen <developer.vanhuy@gmail.com>
2024-03-29 22:41:50 +0700

    Complete responsive website

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # D-SOFT HTML CSS: TEMPLATE 3
+
+### Dự án có Responsive và có sử dụng Javascript
+
+### UI Framework sử dụng trong dự án:
+
+- Bootstrap: Sử dụng grid system 12 column
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,84 @@
-console.log('Hello anh em DSoft');
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const MILLISECONDS_PER_SECOND = 1000;
+const SCROLL_THRESHOLD = 600;
+
+const sidebarContainer = $('.js-sidebar');
+const overlayContainer = $('.js-overlay');
+const subHeaderContainer = $('.js-sub-header');
+const bodyElement = $('body');
+const feedbackVideo = $('.js-feedback-video');
+
+const sidebarBarsButtons = $$('.js-sidebar-bars-btn');
+const sidebarCloseBtn = $('.js-sidebar-close-btn');
+const feedbackPlayVideoBtn = $('.js-feedback-play-video-btn');
+const feedbackPauseVideoBtn = $('.js-feedback-pause-video-btn');
+
+function initApp() {
+  function hiddenScrollBar() {
+    bodyElement.style.overflow = 'hidden';
+  }
+
+  function showScrollBar() {
+    bodyElement.style.overflow = 'unset';
+  }
+
+  function handleShowSidebar() {
+    sidebarContainer.style.transform = 'translateX(0)';
+    overlayContainer.style.display = 'block';
+    hiddenScrollBar();
+  }
+
+  function handleCloseSidebar() {
+    sidebarContainer.style.transform = 'translateX(100%)';
+    overlayContainer.style.display = 'none';
+    showScrollBar();
+  }
+
+  function handlePlayFeedbackVideo() {
+    feedbackVideo.play();
+    feedbackPlayVideoBtn.style.display = 'none';
+    feedbackPauseVideoBtn.style.display = 'flex';
+
+    setTimeout(() => {
+      feedbackPauseVideoBtn.style.display = 'none';
+    }, MILLISECONDS_PER_SECOND);
+  }
+
+  function handlePauseFeedbackVideo() {
+    feedbackVideo.pause();
+    feedbackPlayVideoBtn.style.display = 'flex';
+    feedbackPauseVideoBtn.style.display = 'none';
+  }
+
+  function handleToggleFeedbackVideo() {
+    if (feedbackVideo.paused) {
+      handlePlayFeedbackVideo();
+    } else {
+      handlePauseFeedbackVideo();
+    }
+  }
+
+  function handleShowSubHeader() {
+    if (window.scrollY >= SCROLL_THRESHOLD) {
+      subHeaderContainer.style.top = '0';
+    } else {
+      subHeaderContainer.style.top = '-100%';
+    }
+  }
+
+  // Listen DOM Events
+  sidebarBarsButtons.forEach((sidebarBarsButton) => {
+    sidebarBarsButton.addEventListener('click', handleShowSidebar);
+  });
+  sidebarCloseBtn.addEventListener('click', handleCloseSidebar);
+  overlayContainer.addEventListener('click', handleCloseSidebar);
+  feedbackPlayVideoBtn.addEventListener('click', handlePlayFeedbackVideo);
+  feedbackPauseVideoBtn.addEventListener('click', handlePauseFeedbackVideo);
+
+  feedbackVideo.addEventListener('click', handleToggleFeedbackVideo);
+  window.addEventListener('scroll', handleShowSubHeader);
+}
+
+initApp();
