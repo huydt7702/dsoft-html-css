8a153f0c5686b2f384b4d166a559ac514028aa6b
Huy Nguyen <developer.vanhuy@gmail.com>
2024-03-30 12:04:44 +0700

    Handle prev or next progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,15 @@ const $$ = document.querySelectorAll.bind(document);
 
 const MILLISECONDS_PER_SECOND = 1000;
 const SCROLL_THRESHOLD = 600;
+const ONE_THIRD = 33.33333;
+const NEXT_PROGRESS = 'next';
+const PREV_PROGRESS = 'prev';
 
 const sidebarContainer = $('.js-sidebar');
 const overlayContainer = $('.js-overlay');
 const subHeaderContainer = $('.js-sub-header');
+const progressContainer = $('.js-progress');
+
 const bodyElement = $('body');
 const feedbackVideo = $('.js-feedback-video');
 
@@ -14,6 +19,8 @@ const sidebarBarsButtons = $$('.js-sidebar-bars-btn');
 const sidebarCloseBtn = $('.js-sidebar-close-btn');
 const feedbackPlayVideoBtn = $('.js-feedback-play-video-btn');
 const feedbackPauseVideoBtn = $('.js-feedback-pause-video-btn');
+const prevProgressBtn = $('.js-prev-progress-btn');
+const nextProgressBtn = $('.js-next-progress-btn');
 
 function initApp() {
   function hiddenScrollBar() {
@@ -68,14 +75,29 @@ function initApp() {
     }
   }
 
+  function handleProgress(direction) {
+    let currentWidth = +progressContainer.style.width.replace('%', '');
+    if (direction === NEXT_PROGRESS) {
+      if (currentWidth > 99) return;
+      currentWidth += ONE_THIRD;
+    } else if (direction === PREV_PROGRESS) {
+      currentWidth -= ONE_THIRD;
+    }
+    progressContainer.style.width = currentWidth + '%';
+  }
+
   // Listen DOM Events
   sidebarBarsButtons.forEach((sidebarBarsButton) => {
     sidebarBarsButton.addEventListener('click', handleShowSidebar);
   });
   sidebarCloseBtn.addEventListener('click', handleCloseSidebar);
   overlayContainer.addEventListener('click', handleCloseSidebar);
+
   feedbackPlayVideoBtn.addEventListener('click', handlePlayFeedbackVideo);
   feedbackPauseVideoBtn.addEventListener('click', handlePauseFeedbackVideo);
+
+  nextProgressBtn.addEventListener('click', () => handleProgress(NEXT_PROGRESS));
+  prevProgressBtn.addEventListener('click', () => handleProgress(PREV_PROGRESS));
 
   feedbackVideo.addEventListener('click', handleToggleFeedbackVideo);
   window.addEventListener('scroll', handleShowSubHeader);
